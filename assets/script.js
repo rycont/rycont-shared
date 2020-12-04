e6c542d8b8f0bea42b629fea8d84e389f766495a
rycont <reactdev@kakao.com>
2020-12-04 15:06:54 +0900

    fix : ㅁㄴㅇㄹ

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,2 @@
 document.getElementById('location').appendChild(document.createTextNode(location.pathname))
-document.getElementById('github').setAttribute('href', "https://github.com/rycont/rycont-shared/blob/master" + location.pathname + location.pathname.endsWith('html') ? '' : '.html')
+document.getElementById('github').setAttribute('href', "https://github.com/rycont/rycont-shared/blob/master" + location.pathname + (location.pathname.endsWith('html') ? '' : '.html'))
